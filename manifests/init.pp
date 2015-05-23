a30d0003643f25c8a45fea843763d68885fec465
Scott Coulton <scott.coulton@healthdirect.org.au>
2015-05-23 21:52:04 +1000

    Fixed styling

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,21 +3,14 @@
 class golang(
 
   $base_dir = '/usr/local/go',
-  $version  = 'go1.4.1',
+  $version  = 'go1.4.2',
   $goroot   = '$GOPATH/bin:/usr/local/go/bin:$PATH',
   $workdir  = '/usr/local/'
 ){
-  
-   
-  package {'git':
-  ensure   => installed, 
-  alias    => 'git', 
-  before   => Exec['make GO']
-  } 
-   
+    
   package { 'gcc':
-  ensure   => installed,
-  before   => Exec['make GO']
+  ensure => installed,
+  before => Exec['make GO']
   }
 
   case $::osfamily {
@@ -25,69 +18,69 @@ class golang(
   'RedHat': {
   
   package { 'glibc-devel':
-  ensure   => installed,
-  before   => Exec['make GO']
+  ensure => installed,
+  before => Exec['make GO']
     }
-   }
+  }
   
-  'Debian': { 
+  'Debian': {
   
   package {'libc6-dev':
-  ensure   => installed,
-  before   => Exec['make GO']
+  ensure => installed,
+  before => Exec['make GO']
   } ->
 
   package {'bison':
-  ensure   => installed,
-  before   => Exec['make GO']
+  ensure => installed,
+  before => Exec['make GO']
   } ->
 
   package {'make':
-  ensure   => installed,
-  before   => Exec['make GO']
+  ensure => installed,
+  before => Exec['make GO']
   } ->
 
   package {'build-essential':
-  ensure   => installed,
-  before   => Exec['make GO']
-   }
+  ensure => installed,
+  before => Exec['make GO']
+    }
   }
   
-  default: { notify {"$::osfamily is not supported by this module":}
-   }
+  default: { notify {"${::osfamily} is not supported by this module":}
+    }
   }
 
-  vcsrepo { "${base_dir}":
+  vcsrepo { $base_dir:
   ensure   => present,
   provider => git,
   source   => 'https://github.com/golang/go.git',
   revision => 'master',
   before   => [Exec['make GO'], Exec['checkout go']]
   }
- 
+  
   exec { 'checkout go':
-  path     => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-  command  => "git checkout ${version}",
-  cwd      => '/usr/local/go/', 
-  before   => Exec['make GO'],
-  creates   => '/etc/profile.d/golang.sh'
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  command => "git checkout ${version}",
+  cwd     => '/usr/local/go/',
+  before  => Exec['make GO'],
+  creates => '/etc/profile.d/golang.sh'
   }
 
 
   exec { 'make GO':
-  path     => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-  cwd      => '/usr/local/go/src/',
-  command  => 'sh -c ./all.bash',
-  creates   => '/etc/profile.d/golang.sh'
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  cwd     => '/usr/local/go/src/',
+  command => 'sh -c ./all.bash',
+  creates => '/etc/profile.d/golang.sh'
   }
 
   file { '/etc/profile.d/golang.sh':
-  ensure   => present,
-  content  => template('golang/golang.sh.erb'),
-  owner    => root,
-  group    => root,
-  mode     => 'a+x',
-  require  => Exec['make GO']
-   } 
+  ensure  => present,
+  content => template('golang/golang.sh.erb'),
+  owner   => root,
+  group   => root,
+  mode    => 'a+x',
+  require => Exec['make GO']
+    }
   }
 
